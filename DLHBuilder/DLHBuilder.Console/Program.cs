9818a0e409bf9e9661aa9d4053ac5340c43f7a78
Matt Monks <mattmonks@aplant.com>
2022-03-04 00:16:41 +0000

    Added base functionality for Code Generator

--- DLHBuilder/DLHBuilder.Console/Program.cs
+++ DLHBuilder/DLHBuilder.Console/Program.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using Newtonsoft.Json;
 using System.IO;
 using System.Reflection;
+using DLHBuilder.Generator;
 
 namespace DLHBuilder
 {
@@ -16,19 +17,33 @@ namespace DLHBuilder
     {
         static void Main(string[] args)
         {
+            string template = "Hello $Template.Name$, you are $Template_Age$ years old...";
+            Console.Write("Enter name for Template value: ");
+            object baseobject = new Template() { Name = Console.ReadLine(), Age = 24 };
+            ScriptEngine builder = new ScriptEngine(template, baseobject);
+
+            Console.WriteLine(builder.Render());
+
+            Console.ReadKey();
+        }
+
+        public class Template
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+        }
+
+        List<Type> DataTypes()
+        {
             Assembly syslib = typeof(string).Assembly;
 
             Type[] types = syslib.GetTypes()
                 .Where(x => x.Namespace == "System")
-                .Where(x => x.IsPrimitive || (new string[] { "String", "Object"}).Contains(x.Name))
+                .Where(x => x.IsPrimitive || (new string[] { "String", "Object" }).Contains(x.Name))
                 .ToArray();
 
-            foreach(Type type in types.OrderBy(x => x.Name))
-            {
-                Console.WriteLine(type.Name);
-            }
-
-            Console.ReadKey();
+            return types.ToList();
         }
     }
 }
---
+++ DLHBuilder/DLHBuilder.Generator/ScriptEngine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4;
+using Antlr4.StringTemplate;
+using System.Reflection;
+
+namespace DLHBuilder.Generator
+{
+    public class ScriptEngine
+    {
+        public ScriptEngine(string template, params object[] baseobjects)
+        {
+            Template = template;
+            BaseObjects = baseobjects;
+        }
+
+        string Template { get; set; }
+
+        object[] BaseObjects { get; set; }
+
+        public string Render()
+        {
+            char delimiter = '$';
+            Template output = new Template(Template, delimiter, delimiter);
+
+            foreach(object baseobject in BaseObjects)
+            {
+                if(Template.Contains(string.Format("{0}{1}.", delimiter, baseobject.GetType().Name)))
+                {
+                    output.Add(baseobject.GetType().Name, baseobject);
+                }
+
+                foreach(PropertyInfo property in baseobject.GetType().GetProperties())
+                {
+                    string propertyfullname = string.Format("{0}_{1}", baseobject.GetType().Name, property.Name);
+
+                    if(Template.Contains(propertyfullname))
+                    {
+                        output.Add(propertyfullname, property.GetValue(baseobject));
+                    }
+                }
+            }
+
+            return output.Render();
+        }
+    }
+}
