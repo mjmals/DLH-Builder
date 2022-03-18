a2447c2a2e3e1ea25d89e602ff57eaaff45e903b
Matt Monks <mattmonks@aplant.com>
2022-03-18 15:17:47 +0000

    Added placeholders for EditorGrid

---
+++ DLHBuilder/DLHBuilder.Desktop/UI/EditorGrid/EditorGrid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DLHBuilder.Desktop.UI
+{
+    class EditorGrid : DataGridView
+    {
+        public EditorGrid()
+        {
+            CellValueChanged += OnCellValueChanged;
+        }
+
+        void OnCellValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
