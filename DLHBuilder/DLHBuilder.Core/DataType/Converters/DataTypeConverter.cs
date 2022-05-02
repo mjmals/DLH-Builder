789c6d8e935bf6003f9872c8c349d7b834f0c88b
Matt Monks <mattmonks@aplant.com>
2022-05-02 20:54:30 +0100

    Created Data Type converter classes

---
+++ DLHBuilder/DLHBuilder.Core/DataType/Converters/DataTypeConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using DLHBuilder.DataType.Converters;
+
+namespace DLHBuilder
+{
+    public class DataTypeConverter
+    {
+        public DataTypeConverter(Type sourceConnectionType, string sourceType)
+        {
+            SourceConnectionType = sourceConnectionType;
+            SourceType = sourceType;
+        }
+
+        Type SourceConnectionType { get; set; }
+
+        string SourceType { get; set; }
+
+        IDataTypeConverter GetConverter()
+        {
+            var converters = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(s => s.IsAssignableTo(typeof(IDataTypeConverter)) && !s.IsInterface)
+                .ToArray();
+
+            foreach(Type converterType in converters)
+            {
+                IDataTypeConverter converter = (IDataTypeConverter)Activator.CreateInstance(converterType);
+
+                if (converter.SourceTypes.ContainsKey(SourceConnectionType))
+                {
+                    string[] sourceTypes = converter.SourceTypes[SourceConnectionType];
+
+                    if(sourceTypes.Any(x => SourceType.ToLower().Contains(x)))
+                    {
+                        return converter;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public IDataType GetDataType()
+        {
+            return GetConverter().ConvertSourceType(SourceType);
+        }
+    }
+}
---
+++ DLHBuilder/DLHBuilder.Core/DataType/Converters/SQL/StringDataTypeSQLConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLHBuilder.DataType.Converters.SQL
+{
+    class StringDataTypeSQLConverter : IDataTypeConverter
+    {
+        public Dictionary<Type, string[]> SourceTypes => new Dictionary<Type, string[]>
+        {
+            { typeof(SQLDataConnection), new string[] { "char", "text" } }
+        };
+
+        public IDataType ConvertSourceType(string sourceType)
+        {
+            StringDataType output = new StringDataType();
+            output.IsUnicode = sourceType.ToLower().StartsWith("n") ? true : false;
+            string length = sourceType.Split('(', ')').Where(x => !string.IsNullOrEmpty(x)).ToArray().Last();
+            output.Length = length.ToLower() == "max" ? 8000 : Convert.ToInt32(length);
+
+            return output;
+        }
+    }
+}
