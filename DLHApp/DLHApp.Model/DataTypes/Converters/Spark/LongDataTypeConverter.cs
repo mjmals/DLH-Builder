5e782b44c4ad79c14f15cce37457a7f1b146934a
Matt Monks <mattmonks@aplant.com>
2023-03-30 18:08:45 +0100

    Added long data type

---
+++ DLHApp/DLHApp.Model/DataTypes/Converters/Spark/LongDataTypeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLHApp.Model.DataTypes.Converters.Spark
+{
+    public class LongDataTypeConverter : SparkDataTypeConverter, IDataTypeConverter
+    {
+        public override string[] SourceTypeNames => new string[] { "LongType" };
+
+        public override Type[] DataTypes => new Type[] { typeof(LongDataType) };
+
+        public override IDataType Import(string dataType)
+        {
+            return new LongDataType();
+        }
+
+        public override string Export(IDataType dataType)
+        {
+            return "LongType()";
+        }
+    }
+}
