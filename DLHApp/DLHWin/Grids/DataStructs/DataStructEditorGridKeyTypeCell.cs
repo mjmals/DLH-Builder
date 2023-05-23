50297ae4957bf7f1f7d1592860aea9008cdf57a3
monksm <mjmals@users.noreply.github.com>
2023-05-23 20:13:39 +0100

    Fixed Key Type cell in Data Struct editor

--- DLHApp/DLHWin/Grids/DataStructs/DataStructEditorGridKeyTypeCell.cs
+++ DLHApp/DLHWin/Grids/DataStructs/DataStructEditorGridKeyTypeCell.cs
@@ -11,15 +11,49 @@ namespace DLHWin.Grids.DataStructs
     {
         public override void SetValue(object value)
         {
-            DataStructFieldKeyTypeCollection keyTypes = (DataStructFieldKeyTypeCollection)value;
-
-            if (keyTypes != null && keyTypes.Count > 0)
+            if (value is DataStructFieldKeyTypeCollection)
             {
-                Value = string.Join(",", keyTypes.Select(x => x.ToString()).ToArray());
+                DataStructFieldKeyTypeCollection keyTypes = (DataStructFieldKeyTypeCollection)value;
+
+                if (keyTypes != null && keyTypes.Count > 0)
+                {
+                    Value = string.Join(",", keyTypes.Select(x => x.ToString()).ToArray());
+                    return;
+                }
+            }
+
+            if (value is string)
+            {
+                Value = value.ToString();
                 return;
             }
 
             Value = string.Empty;
+        }
+
+        public override void UpdateBaseProperty(object baseObject)
+        {
+            string[] keyTypeStrings = Value.ToString().Split(",");
+            List<DataStructFieldKeyType> keyTypes = new List<DataStructFieldKeyType>();
+
+            foreach(string keyTypeString in keyTypeStrings.Where(x => !string.IsNullOrEmpty(x)))
+            {
+                try
+                {
+                    DataStructFieldKeyType keyType = Enum.Parse<DataStructFieldKeyType>(keyTypeString.Trim());
+                    keyTypes.Add(keyType);
+                }
+                catch
+                {
+                    MessageBox.Show(string.Format("\"{0}\" is not a valid Key Type.", keyTypeString.Trim()));
+                    return;
+                }
+            }
+
+            DataStructField field = (DataStructField)baseObject;
+            if (field.KeyTypes == null) field.KeyTypes = new DataStructFieldKeyTypeCollection();
+            field.KeyTypes.Clear();
+            field.KeyTypes.AddRange(keyTypes);
         }
     }
 }
